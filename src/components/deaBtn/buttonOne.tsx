8712700a30b9e88132af8e600cf6cc74f95c6f51
Mange99 <magnus.bryntheson@hotmail.com>
2022-04-05 23:14:05 +0200

    Magnus knapp with Chakra UI

---
+++ src/components/deaBtn/buttonOne.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import "../deaBtn/buttonOneCss.css";
+
+function onClickOne() {
+  alert("Deaa");
+}
+
+function ButtonOne() {
+  return <button className="buttonOne">Deaa</button>;
+}
+export default ButtonOne;
