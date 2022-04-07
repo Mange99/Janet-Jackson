065d7a1e7210607ca2c32a44bcaa5c13ea6078b8
DIONAPERSSON <42834340+DIONAPERSSON@users.noreply.github.com>
2022-04-07 15:08:39 +0200

    Sara Button
    I added a button and am now trying to upload it to git

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ButtonOne from "./components/deaBtn/buttonOne";
 import MagnusBtn from "./components/MagnusBtn";
 import TbButton from "./components/TorbjornBtn";
 import HeddaBtn from "./components/HeddaBtn";
+import SaraBtn from "./components/SaraBtn";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <MagnusBtn />
       <TbButton />
       <HeddaBtn />
+      <SaraBtn />
       {/* Lägg in en egen knapp me ert namn här under*/}
     </Flex>
   );
