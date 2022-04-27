706a03dac484a5e7f305766e83589cd6d43a3be5
torbie <lord.tobbe@gmail.com>
2022-04-28 01:07:16 +0200

    review changes done

--- src/components/calculatorCards/macroCalculator.tsx
+++ src/components/calculatorCards/macroCalculator.tsx
@@ -10,25 +10,66 @@ const fatPercentageHigh: number = 0.35
 
 export const MacroCalculator = () => {
 
-    const [calorieIntake, setCalorieIntake] = useState(0);
+    //const [calorieIntake, setCalorieIntake] = useState(0);
+    const [showMacros, setShowMacros] = useState(false);
+    const [oldCalorieIntake, setOldCalorieIntake] = useState(0);
+    /*
     const [carbsLow, setCarbsLow] = useState(0);
     const [carbsHigh, setCarbsHigh] = useState(0);
     const [proteinLow, setProteinLow] = useState(0);
     const [proteinHigh, setProteinHigh] = useState(0);
     const [fatLow, setFatLow] = useState(0);
     const [fatHigh, setFatHigh] = useState(0);
-    const [showMacros, setShowMacros] = useState(false);
-    const [macroArray, setMacroArray] = useState([]);
-    const [state, dispatch] = useReducer(reducer, initialState);
+    */
+    type Action = 
+        | {type: "Calculate"}
+        | {type: "Calories", calories: number};
 
-    const action = {
-        type: "Calculate"
-    };
 
-    const reducer = (state, action) => {
-
+    type State =  {
+      calorieIntake: number,
+      carbsLow: number,
+      carbsHigh: number,
+      proteinLow: number,
+      proteinHigh: number,
+      fatLow: number,
+      fatHigh: number
+    }
+    
+    const initialState: State = {
+      calorieIntake: 0,
+      carbsLow: 0,
+      carbsHigh: 0,
+      proteinLow: 0,
+      proteinHigh: 0,
+      fatLow: 0,
+      fatHigh: 0
     }
 
+    const reducer = (state: State, action: Action): State => {
+        switch(action.type) {
+          case 'Calculate':
+            return {
+              ...state,
+              carbsLow: Math.round(state.calorieIntake*carbPercentageLow), 
+              carbsHigh: Math.round(state.calorieIntake*carbPercentageHigh),
+              proteinLow: Math.round(state.calorieIntake*proteinPercentageLow),
+              proteinHigh: Math.round(state.calorieIntake*proteinPercentageHigh),
+              fatLow: Math.round(state.calorieIntake*fatPercentageLow),
+              fatHigh: Math.round(state.calorieIntake*fatPercentageHigh)
+             }
+           case 'Calories':   
+            return {
+              ...state,
+              calorieIntake: action.calories
+            }
+             
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    /*
     const macroCalc = () => {
         setCarbsLow(Math.round(calorieIntake*carbPercentageLow));
         setCarbsHigh(Math.round(calorieIntake*carbPercentageHigh));
@@ -36,9 +77,8 @@ export const MacroCalculator = () => {
         setProteinHigh(Math.round(calorieIntake*proteinPercentageHigh));
         setFatLow(Math.round(calorieIntake*fatPercentageLow));
         setFatHigh(Math.round(calorieIntake*fatPercentageHigh));
-        setShowMacros(true);
     }
-
+*/
     return (
         <>
           <Card style={{ width: "20rem", height: "18rem" }}>
@@ -52,7 +92,7 @@ export const MacroCalculator = () => {
                       <Col>
                         <Form.Group controlId="calorieIntake">
                           <Form.Control
-                            onChange={(e) => (setCalorieIntake(Number(e.target.value)))}
+                            onChange={(e) => (dispatch({type: "Calories", calories: Number(e.target.value)}))}
                             min="0"
                             max="30000"
                             type="number"
@@ -65,16 +105,16 @@ export const MacroCalculator = () => {
                 </Form.Group>
               </Form>
     
-              <Button onClick={() => macroCalc()} variant="primary">
+              <Button onClick={() => dispatch({type: "Calculate"})} variant="primary">
                 Calculate
               </Button>
             </Card.Body>
             <Card.Body>
-                 {showMacros && (calorieIntake > 0) && 
+                 {showMacros && 
                     <div className="mb-4">
-                        <h3 className="mb-2">Carbohydrates: {carbsLow} - {carbsHigh} Grams/Day</h3>
-                        <h3 className="mb-2">Protein: {proteinLow} - {proteinHigh} Grams/Day</h3>
-                        <h3>Fat: {fatLow} - {fatHigh} Grams/Day</h3>
+                        <h3 className="mb-2">Carbohydrates: {state.carbsLow} - {state.carbsHigh} Grams/Day</h3>
+                        <h3 className="mb-2">Protein: {state.proteinLow} - {state.proteinHigh} Grams/Day</h3>
+                        <h3>Fat: {state.fatLow} - {state.fatHigh} Grams/Day</h3>
                     </div>
                 }
             </Card.Body>
