0ad976488511c554e649386be6c63346945ad57e
Mange99 <magnus.bryntheson@hotmail.com>
2022-04-29 19:48:58 +0200

    tbRe

--- src/components/recipeCardGrid/recipeCardGrid.tsx
+++ src/components/recipeCardGrid/recipeCardGrid.tsx
@@ -1,34 +1,39 @@
+import { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import {sendApiRequest} from "../../recipeApiHandler/recipeApiHandler";
-import {recipe} from "../recipeInterfaces"
+//import { sendApiRequest } from "../../recipeApiHandler/recipeApiHandler";
+import { recipe, recipes } from "../recipeInterfaces";
 
-const foodRecipes:number [] = [
-    1,3,4,5,6
-];
+export function RecipeCardGrid() {
+  const [recipes, setRecipes] = useState<recipes[]>([]);
 
-export function RecipeCardGrid(){
-    return (
-        <div>
-            <Row xs={1} md ={3} lg={5}>
-                <Col >
-              {sendApiRequest.map((recipe:recipe)=>(
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        APP_ID: "3e55957f",
+        API_KEY: "154526ece32bedbe18ee7788fd6c7e8e",
+      },
+    };
 
-                  <p>{number}</p>
-
-
-
-              ))}
-
-
-           
-                </Col>
-            </Row>
-
-
-        </div>
+    fetch(
+      "https://api.edamam.com/api/recipes/v2?app_id=3e55957f&app_key=154526ece32bedbe18ee7788fd6c7e8e&type=public&q=pizza",
+      options
     )
+      .then((response) => response.json())
+      .then((response) => {
+        setRecipes(response);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
+  console.log(recipes);
+  Object.values(recipes).map((recepie: recipes) => {
+    console.log(recepie.count);
+  });
 
-
-
+  return (
+    <div>
+      <Row xs={1} md={3} lg={5}></Row>
+    </div>
+  );
 }
--- src/components/recipeInterfaces.ts
+++ src/components/recipeInterfaces.ts
@@ -1,10 +1,13 @@
-
-
-export interface recipe{
-    calories: number
-    image: string
-    label: string
-    totalTime: number
-
-    
+export interface recipes {
+  from: number;
+  to: number;
+  count: number;
+  _links: [];
+  hits: [];
+}
+export interface recipe {
+  calories: number;
+  image: string;
+  label: string;
+  totalTime: number;
 }
