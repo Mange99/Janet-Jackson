d6abb2a1ddf527b8f1e5550ec84a996ca5d4ea10
DIONAPERSSON <42834340+DIONAPERSSON@users.noreply.github.com>
2022-04-29 12:57:22 +0200

    TipsPage
    The first of the tips page done! :)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { HeaderComponent } from "./components/header/headerComponent";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ExercisePage } from "../src/components/exercisePage/exercisePage";
 import { CalculatorPage } from "../src/components/calculatorPage/calculatorPage";
+import { TipsPage } from "../src/components/tipsPage/tipsPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HeaderComponent />}>
           <Route path="exercise" element={<ExercisePage />} />
           <Route path="calculators" element={<CalculatorPage />} />
+          <Route path="tips" element={<TipsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
